e1ee1fcab92b9c6ad5048a7968cd4bb7680c28a0
aspizu <aspizu@protonmail.com>
2025-03-01 15:56:33 +0530

    feat: remove allotment in main editor layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import {Allotment} from "allotment"
 import "allotment/dist/style.css"
 import Editor from "./components/Editor"
 import NavBar from "./components/NavBar"
@@ -8,14 +7,10 @@ export default function App() {
     return (
         <>
             <NavBar />
-            <Allotment className="flex-[1_1_auto]">
-                <Allotment.Pane minSize={128} preferredSize={192}>
-                    <Palette />
-                </Allotment.Pane>
-                <Allotment.Pane snap>
-                    <Editor />
-                </Allotment.Pane>
-            </Allotment>
+            <div className="flex-[1_1_auto] grid grid-cols=[192px_1fr]">
+                <Palette />
+                <Editor />
+            </div>
         </>
     )
 }
