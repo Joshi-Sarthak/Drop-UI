4c10d5fa681b59842080b080721fb5d9f54f7092
Indrajeet-007 <indrajeetbhosale011@gmail.com>
2025-03-01 23:38:18 +0530

    feat: add routes and back button from preview page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import "allotment/dist/style.css"
-import Editor from "./components/Editor"
-import Footer from "./components/Footer"
-import NavBar from "./components/NavBar"
-import Palette from "./components/Palette"
-import RightPanel from "./components/RightPanel"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "allotment/dist/style.css";
+import Editor from "./components/Editor";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Palette from "./components/Palette";
+import RightPanel from "./components/RightPanel";
+import Preview from "./components/Preview"; // Import Preview Page
 
 export default function App() {
     return (
-        <>
+        <Router>
             <NavBar />
-            <div className="flex h-screen">
-                <div className="w-1/5 bg-gray-100 flex-shrink-0">
-                    <Palette />
-                </div>
-                <div className="flex-1">
-                    <Editor />
-                </div>
-                <div className="w-1/5 bg-gray-100 flex-shrink-0">
-                    <RightPanel />
-                </div>
-            </div>
+            <Routes>
+                {/* Main Editor Page */}
+                <Route
+                    path="/"
+                    element={
+                        <div className="flex h-screen">
+                            <div className="w-1/5 bg-gray-100 flex-shrink-0">
+                                <Palette />
+                            </div>
+                            <div className="flex-1">
+                                <Editor />
+                            </div>
+                            <div className="w-1/5 bg-gray-100 flex-shrink-0">
+                                <RightPanel />
+                            </div>
+                        </div>
+                    }
+                />
+                {/* Preview Page */}
+                <Route path="/preview" element={<Preview />} />
+            </Routes>
             <Footer />
-        </>
-    )
+        </Router>
+    );
 }
