02564521f489fa16d7be828ae2762cad91693f74
aspizu <aspizu@protonmail.com>
2025-03-01 15:12:39 +0530

    feat: add sidebar grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <>
             <NavBar />
-            <div className="flex flex-[1_1_auto]">
+            <div className="flex-[1_1_auto] grid grid-cols-[250px_1fr]">
                 <Palette />
                 <Editor />
             </div>
--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -1,3 +1,3 @@
 export default function Palette() {
-    return null
+    return <div>Palette</div>
 }
