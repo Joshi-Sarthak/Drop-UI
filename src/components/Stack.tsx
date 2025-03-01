e4e8bd02dbc5cfda2cec07d54b8afbca44a31ba5
aspizu <aspizu@protonmail.com>
2025-03-01 20:26:23 +0530

    feat: allow dragging-in new components between stack items

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,3 +1,7 @@
+import {
+    attachClosestEdge,
+    extractClosestEdge,
+} from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge"
 import {dropTargetForElements} from "@atlaskit/pragmatic-drag-and-drop/element/adapter"
 import {useEffect, useRef, useState} from "react"
 import {StaticComponent} from "../store"
@@ -20,13 +24,37 @@ function StackItem({
         if (!ref.current) return
         return dropTargetForElements({
             element: ref.current,
+            getData: ({input, element}) =>
+                attachClosestEdge(
+                    {index},
+                    {
+                        input,
+                        element,
+                        allowedEdges: ["left", "right"],
+                    },
+                ),
             onDragEnter: () => setIsDraggedOver(true),
             onDragLeave: () => setIsDraggedOver(false),
-            onDrop: ({source}) => {
-                const newComponents = components.map((component, i) =>
-                    i === index ? (source.data as any) : component,
-                )
-                setComponents(newComponents)
+            onDrop: ({self, source}) => {
+                const closestEdgdeOfTarget = extractClosestEdge(self.data)
+                if (closestEdgdeOfTarget == "left") {
+                    setComponents([
+                        ...components.slice(0, index),
+                        source.data as any,
+                        ...components.slice(index),
+                    ])
+                } else if (closestEdgdeOfTarget == "right") {
+                    setComponents([
+                        ...components.slice(0, index + 1),
+                        source.data as any,
+                        ...components.slice(index + 1),
+                    ])
+                } else {
+                    const newComponents = components.map((component, i) =>
+                        i === index ? (source.data as any) : component,
+                    )
+                    setComponents(newComponents)
+                }
                 setIsDraggedOver(false)
             },
         })
