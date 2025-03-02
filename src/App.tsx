176c159a250663c7d75153f76a697acf3853323b
Sarthak Joshi <sarthaksj02@gmail.com>
2025-03-02 08:07:49 +0530

    feat: add sidebar and demo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "allotment/dist/style.css";
-import Editor from "./components/Editor";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import Palette from "./components/Palette";
-import RightPanel from "./components/RightPanel";
-import Preview from "./components/Preview"; // Import Preview Page
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import "allotment/dist/style.css"
+import Editor from "./components/Editor"
+import Footer from "./components/Footer"
+import NavBar from "./components/NavBar"
+import Palette from "./components/Palette"
+import RightPanel from "./components/RightPanel"
+import Preview from "./components/Preview" // Import Preview Page
 
 export default function App() {
     return (
         <Router>
-            <NavBar />
             <Routes>
                 {/* Main Editor Page */}
                 <Route
                     path="/"
                     element={
-                        <div className="flex h-screen">
-                            <div className="w-1/5 bg-gray-100 flex-shrink-0">
-                                <Palette />
+                        <>
+                            <NavBar />
+                            <div className="flex h-screen">
+                                <div className="w-1/5 bg-gray-100 flex-shrink-0">
+                                    <Palette />
+                                </div>
+                                <div className="flex-1">
+                                    <Editor />
+                                </div>
+                                <div className="w-1/5 bg-gray-100 flex-shrink-0">
+                                    <RightPanel />
+                                </div>
                             </div>
-                            <div className="flex-1">
-                                <Editor />
-                            </div>
-                            <div className="w-1/5 bg-gray-100 flex-shrink-0">
-                                <RightPanel />
-                            </div>
-                        </div>
+                            <Footer />
+                        </>
                     }
                 />
                 {/* Preview Page */}
                 <Route path="/preview" element={<Preview />} />
             </Routes>
-            <Footer />
         </Router>
-    );
+    )
 }
---
+++ src/library/Buttons/Button1.tsx
@@ -0,0 +1,5 @@
+const Button1 = `<button title="Save" class="cursor-pointer flex items-center justify-center text-center fill-lime-400 bg-lime-950 hover:bg-lime-900 active:border active:border-lime-400 rounded-md duration-100 p-2">
+            
+            <span class="text-sm text-lime-400 font-bold pr-1">Submit</span>
+        </button>`
+export default Button1
