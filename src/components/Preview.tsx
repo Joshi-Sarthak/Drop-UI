d754a517fc9adaea08f773528e37d20daaafbf26
Indrajeet-007 <indrajeetbhosale011@gmail.com>
2025-03-02 09:52:49 +0530

    style: Preview page

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,62 +1,71 @@
-import useStore from "../store"
-import Component from "./Component"
+import useStore from "../store";
+import Component from "./Component";
 
 export default function Preview() {
-    const {headerStack, leftStack, rightStack, footerStack} = useStore(
-        (state) => state.project,
-    )
+    const { headerStack, leftStack, rightStack, footerStack } = useStore(
+        (state) => state.project
+    );
 
     const isEmpty =
         headerStack.length === 0 &&
         leftStack.length === 0 &&
         rightStack.length === 0 &&
-        footerStack.length === 0
+        footerStack.length === 0;
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
-            <div className="w-[100%] h-[100%] flex flex-col bg-gradient-to-b from-gray-50 to-gray-200 shadow-lg rounded-lg overflow-auto p-6">
-                {isEmpty ?
-                    <div className="text-black text-center p-6">
-                        No elements on the palette. Drag and drop components to get
-                        started!
+        <div className="mt-4 mb-4 ml-4 h-screen flex flex-col bg-gray-100">
+            <div className="w-[99%] h-full flex flex-col bg-gradient-to-b from-gray-200 to-gray-300 shadow-lg rounded-lg overflow-auto">
+                {isEmpty ? (
+                    <div className="text-black text-center p-4">
+                        No elements on the palette. Drag and drop components to get started!
                     </div>
-                :   <>
+                ) : (
+                    <>
+                        {/* Header Stack */}
                         {headerStack.length > 0 && (
-                            <div className="p-4 flex flex-row flex-wrap gap-2">
+                            <div className="w-full p-4">
                                 {headerStack.map((comp, index) => (
                                     <Component key={index} {...comp} />
                                 ))}
                             </div>
                         )}
 
-                        <div className="flex flex-wrap justify-center gap-4">
-                            {leftStack.length > 0 && (
-                                <div className="p-4 flex flex-col gap-2">
-                                    {leftStack.map((comp, index) => (
+                        {/* Main Content with Left & Right Stacks */}
+                        <div className="flex flex-grow w-full p-4">
+                            {/* Left Stack */}
+                            <div className="w-1/2 p-4 min-h-[300px]">
+                                {leftStack.length > 0 ? (
+                                    leftStack.map((comp, index) => (
                                         <Component key={index} {...comp} />
-                                    ))}
-                                </div>
-                            )}
+                                    ))
+                                ) : (
+                                    <p className="text-gray-500">Drop any component here</p>
+                                )}
+                            </div>
 
-                            {rightStack.length > 0 && (
-                                <div className="p-4 flex flex-col gap-2">
-                                    {rightStack.map((comp, index) => (
+                            {/* Right Stack */}
+                            <div className="w-1/2 p-4 min-h-[300px]">
+                                {rightStack.length > 0 ? (
+                                    rightStack.map((comp, index) => (
                                         <Component key={index} {...comp} />
-                                    ))}
-                                </div>
-                            )}
+                                    ))
+                                ) : (
+                                    <p className="text-gray-500">Drop any component here</p>
+                                )}
+                            </div>
                         </div>
 
+                        {/* Footer Stack */}
                         {footerStack.length > 0 && (
-                            <div className="p-4 flex flex-row flex-wrap gap-2">
+                            <div className="w-full p-4">
                                 {footerStack.map((comp, index) => (
                                     <Component key={index} {...comp} />
                                 ))}
                             </div>
                         )}
                     </>
-                }
+                )}
             </div>
         </div>
-    )
+    );
 }
