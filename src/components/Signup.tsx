8fba238f3a740a711fccf12603615f1921ea699c
Sarthak Joshi <sarthaksj02@gmail.com>
2025-03-08 18:58:23 +0530

    styles: add signup and login

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,6 +1,8 @@
 import {useState, ChangeEvent, FormEvent, useContext} from "react"
 import {Link} from "react-router-dom"
 import {AuthContext} from "../context/AuthContext"
+import {AnimatedGridPattern} from "./magicui/animated-grid-pattern"
+import {cn} from "@/lib/utils"
 
 const SignUp = () => {
     const {login} = useContext(AuthContext)!
@@ -53,40 +55,53 @@ const SignUp = () => {
 
     return (
         <div className="h-screen flex items-center justify-center bg-gray-50">
-            <div className="bg-white p-8 rounded shadow-md w-96">
-                <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
+            <div className="absolute flex h-[80vh] w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20">
+                <AnimatedGridPattern
+                    numSquares={30}
+                    maxOpacity={0.5}
+                    duration={1}
+                    repeatDelay={1}
+                    className={cn(
+                        "[mask-image:radial-gradient(1200px_circle_at_center,white,transparent)]",
+                        "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+                    )}
+                />
+            </div>
+
+            <div className="bg-neutral-100/50 p-8  shadow-md w-96 border border-neutral-400 rounded-3xl z-10">
+                <h1 className="text-2xl font-semibold mb-4 text-center">Sign Up</h1>
                 <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                     <input
                         type="text"
                         name="username"
                         placeholder="Username"
-                        className="p-2 border rounded"
+                        className="p-2 border border-stone-400 rounded-lg bg-neutral-50"
                         onChange={handleChange}
                     />
                     <input
                         type="email"
                         name="email"
                         placeholder="Email"
-                        className="p-2 border rounded"
+                        className="p-2 border border-stone-400 rounded-lg bg-neutral-50"
                         onChange={handleChange}
                     />
                     <input
                         type="password"
                         name="password"
                         placeholder="Password"
-                        className="p-2 border rounded"
+                        className="p-2 border border-stone-400 rounded-lg bg-neutral-50"
                         onChange={handleChange}
                     />
                     <button
                         type="submit"
-                        className="bg-blue-500 text-white p-2 rounded"
+                        className="bg-stone-900 rounded-3xl hover:bg-stone-950 text-white font-semibold transition-all duration-200 p-2"
                         disabled={loading}
                     >
                         {loading ? "Signing up..." : "Sign Up"}
                     </button>
                 </form>
                 <div className="mt-4">
-                    <Link to="/login" className="text-blue-500">
+                    <Link to="/login" className="text-stone-900 hover:underline">
                         Already have an account? Sign in
                     </Link>
                 </div>
