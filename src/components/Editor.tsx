62ca37e7e3545deed310a83c917370f120e5776e
Sarthak Joshi <sarthaksj02@gmail.com>
2025-03-08 19:45:35 +0530

    feats

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,6 +5,7 @@ import {Block} from "./block"
 import Project from "./Project"
 import RightPanel from "./RightPanel"
 import {Button} from "./ui/button"
+import {Link} from "react-router-dom"
 
 export default function Editor() {
     const ref = useRef<HTMLDivElement>(null)
@@ -115,48 +116,56 @@ export default function Editor() {
     return (
         <div ref={ref} className="flex flex-row h-full w-full">
             <div className="flex flex-col p-2 gap-2 w-10/12 border border-neutral-200 rounded-2xl">
-                <div>
-                    <Button
-                        size="icon"
-                        variant={isSelecting ? "default" : "ghost"}
-                        onClick={() => {
-                            if (isSelecting) {
-                                stopSelecting()
-                            } else {
-                                startSelecting()
-                            }
-                        }}
-                    >
-                        <SquareMousePointer />
-                    </Button>
-                    <Button
-                        size="icon"
-                        variant="ghost"
-                        disabled={!isSelecting}
-                        onClick={deleteSelection}
-                    >
-                        <Trash />
-                    </Button>
-                    <Button
-                        size="icon"
-                        variant={isEditing ? "default" : "ghost"}
-                        disabled={!isSelecting}
-                        onClick={() => {
-                            setIsEditing(!isEditing)
-                            if (isEditing && editingBlock) {
-                                replaceSelection(editingBlock)
-                                stopSelecting()
-                            }
-                        }}
-                    >
-                        <Edit />
-                    </Button>
-                    <Button size="icon" variant="ghost" onClick={onSave}>
-                        <Save />
-                    </Button>
-                    <Button size="icon" variant="ghost" onClick={onLoad}>
-                        <Upload />
-                    </Button>
+                <div className="flex flex-row justify-between items-center">
+                    <div>
+                        {" "}
+                        <Button
+                            size="icon"
+                            variant={isSelecting ? "default" : "ghost"}
+                            onClick={() => {
+                                if (isSelecting) {
+                                    stopSelecting()
+                                } else {
+                                    startSelecting()
+                                }
+                            }}
+                        >
+                            <SquareMousePointer />
+                        </Button>
+                        <Button
+                            size="icon"
+                            variant="ghost"
+                            disabled={!isSelecting}
+                            onClick={deleteSelection}
+                        >
+                            <Trash />
+                        </Button>
+                        <Button
+                            size="icon"
+                            variant={isEditing ? "default" : "ghost"}
+                            disabled={!isSelecting}
+                            onClick={() => {
+                                setIsEditing(!isEditing)
+                                if (isEditing && editingBlock) {
+                                    replaceSelection(editingBlock)
+                                    stopSelecting()
+                                }
+                            }}
+                        >
+                            <Edit />
+                        </Button>
+                        <Button size="icon" variant="ghost" onClick={onSave}>
+                            <Save />
+                        </Button>
+                        <Button size="icon" variant="ghost" onClick={onLoad}>
+                            <Upload />
+                        </Button>
+                    </div>
+                    <Link to="/preview">
+                        <div className="mr-4 text-sm font-semibold transition-all duration-200 hover:underline">
+                            Preview
+                        </div>
+                    </Link>
                 </div>
                 <div className="w-full h-full">
                     <Project />
