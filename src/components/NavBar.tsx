eab4aeab0b5f47b4ef373a0ab54ed3b974c4018e
Sarthak Joshi <sarthaksj02@gmail.com>
2025-03-08 18:58:58 +0530

    fix: navbar bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,7 @@
-import {useNavigate} from "react-router-dom"
+import {Link} from "react-router-dom"
 import favicon from "../../public/favicon.svg"
 
 export default function NavBar() {
-    const navigate = useNavigate();
     return (
         <nav className="flex items-center justify-between px-6 py-4 backdrop-blur-lg">
             <Link to="/">
