dbfe27eac93038bfaa2d6d953d774d9105221f2a
Indrajeet-007 <indrajeetbhosale011@gmail.com>
2025-03-08 18:45:53 +0530

    feat : add preview

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
 
-                <Route path="/preview" element={<PrivateRoute />}></Route>
+                <Route path="/preview" element={<><Preview/></>}></Route>
                 <Route
                     path="/editor"
                     element={
